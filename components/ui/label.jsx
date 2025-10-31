f6607d8b59f0da2ddde3d529b96745393e531824
ShopAkash <sharma.akash1420@gmail.com>
2025-10-31 20:39:11 +0530

    delete

--- components/ui/label.jsx
+++
@@ -1,7 +0,0 @@
-import clsx from 'clsx';
-
-export function Label({ className, ...props }) {
-  return <label className={clsx('mb-1 block text-sm font-medium text-gray-700', className)} {...props} />;
-}
-
-
